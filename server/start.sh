7c80571f008ae6148edc213c39bbb80f523ce99d
SafeChat Developer <developer@safechat.app>
2025-11-28 15:16:56 +0530

    fix: Simplify Railway configuration and update start script

--- server/start.sh
+++ server/start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# SafeChat Server Startup Script for Railway
-
-# Install dependencies if needed
-if [ ! -d "node_modules" ]; then
-  npm install
-fi
-
-# Start the server
+npm install
 npm start
